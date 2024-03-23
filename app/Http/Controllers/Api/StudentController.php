c2abd5ad83e09be6597d167546145e3d531313de
feisalmombo <feisalmombo29@gmail.com>
2024-03-23 17:58:54 +0300

    modify studentcontroller

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -5,6 +5,7 @@ use App\Student;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
@@ -23,6 +24,47 @@ class StudentController extends Controller
                 'status' => 404,
                 'message' => 'No Records Found'
             ], 404);
+        }
+    }
+
+
+    public function store(Request $request)
+    {
+        $validattor = Validator::make($request->all(), [
+            'name' => 'required|string|max:91',
+            'course' => 'required|string|max:91',
+            'email' => 'required|email|max:91',
+            'phone' => 'required|digits:10',
+        ]);
+
+        if($validattor->fails()) {
+
+            return response()->json([
+                'status' => 422,
+                'errors' => $validattor->messages()
+            ], 422);
+        }else {
+
+            $student = Student::create([
+                'name' => $request->name,
+                'course' => $request->course,
+                'email' => $request->email,
+                'phone' => $request->phone,
+            ]);
+
+            if($student) {
+
+                return response()->json([
+                    'status' => 200,
+                    'message' => "Student Created Successfully"
+                ], 200);
+            }else {
+
+                return response()->json([
+                    'status' => 500,
+                    'message' => "Something Went Wrong"
+                ], 500);
+            }
         }
     }
 }
