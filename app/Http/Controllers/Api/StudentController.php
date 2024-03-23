e68169e696d45b247392565882e96628860578eb
feisalmombo <feisalmombo29@gmail.com>
2024-03-23 16:28:36 +0300

    modify index function on studentcontroller

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+use App\Student;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -9,6 +10,19 @@ class StudentController extends Controller
 {
     public function index()
     {
-        return 'I am Here';
+        $students = Student::all();
+
+        if($students->count() > 0){
+
+            return response()->json([
+                'status' => 200,
+                'students' => $students
+            ], 200);
+        }else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No Records Found'
+            ], 404);
+        }
     }
 }
