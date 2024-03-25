108a8c80c4786e2109f23d8361f3622350f169c3
feisalmombo <feisalmombo29@gmail.com>
2024-03-25 15:44:18 +0300

    create api route for delete

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,4 @@ Route::post('students', [StudentController::class, 'store']);
 Route::get('students/{id}', [StudentController::class, 'show']);
 Route::get('students/{id}/edit', [StudentController::class, 'edit']);
 Route::put('students/{id}/edit', [StudentController::class, 'update']);
+Route::delete('students/{id}/delete', [StudentController::class, 'destroy']);
