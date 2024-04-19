e5f3fb7002dcbb36400db8d1cba944e09edd8503
feisalmombo <feisalmombo29@gmail.com>
2024-04-19 16:17:28 +0300

    WIP

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,8 +9,22 @@ use Hash;
 
 class LoginController extends Controller
 {
-    public function check()
+    public function check(Request $request)
     {
-        //
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+               ]);
+
+               if (Auth::attempt($credentials))
+               {
+                  return response()->json([ 'status' => true,
+                    'message' => "Success"
+               ]);
+               }
+                   return response()->json(['status' => false,
+                    'message' => "Fail"
+
+               ]);
     }
 }
